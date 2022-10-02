2d7c477d7511fc241ed1a91667956d4cdde72b3b
sanjeetkhanuja <sanjeetkhanuja@yahoo.com>
2022-10-02 23:07:52 +0530

    first commit

---
+++ SampleRESTAPI.py
@@ -0,0 +1,19 @@
+from flask import Flask, jsonify, request
+import json
+
+app = Flask(__name__)
+
+questions = [
+  { 'description': 'which is capital of India    ?', 'answer': 'DELHI' }
+]
+
+@app.route('/question')
+def get_question():
+  return jsonify(questions)
+
+@app.route('/')
+def get_health():
+  return 'OK', 200
+
+if __name__ == "__main__":
+    app.run(threaded=True, host='0.0.0.0', port=5000, use_reloader=True)
